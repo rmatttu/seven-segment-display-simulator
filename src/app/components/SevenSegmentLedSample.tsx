9eeb0d95977ece4361765539eb41158b6137bc33
mggggk <mggggk@gmail.com>
2025-01-25 16:42:20 +0900

    Fix lint errors

--- src/app/components/SevenSegmentLedSample.tsx
+++ src/app/components/SevenSegmentLedSample.tsx
@@ -1,25 +1,11 @@
 'use client';
 
-import React, { ReactNode, useRef } from 'react'
+import React from 'react'
 import Image from "next/image";
 
 // [File:7-segment none.svg - Wikimedia Commons](https://commons.wikimedia.org/wiki/File:7-segment_none.svg?uselang=ja)
 
 const CustomHeader = () => {
-  const textRef = useRef(null)
-  const titileStyle = {
-    margin: "0px",
-    color: "#3d84a8"
-  };
-
-  const testStyle = {
-    normal: {
-      "#a svg": {
-        fill: "#1470cc"
-      }
-    },
-  }
-
   return (
     <div>
       <p>hello.</p>
