950f5df06092beea4af7438f792a4789eee14fd1
mggggk <mggggk@gmail.com>
2025-01-26 15:09:48 +0900

    Add CircleSvg

---
+++ src/app/components/CircleSvg.tsx
@@ -0,0 +1,29 @@
+'use client'
+
+import React, { FC, useState, MouseEventHandler } from 'react';
+
+interface CircleProps {
+  color1: string;
+  color2: string;
+}
+
+const circleSvg: FC<CircleProps> = ({ color1, color2 }) => {
+  const [clickCount, setClickCount] = useState(0);
+  const currentColor = clickCount % 2 === 0 ? color1 : color2
+
+  const onClick = () => {
+    setClickCount(clickCount + 1)
+  }
+
+  return <svg width="100" height="100">
+    <circle
+      cx="50"
+      cy="50"
+      r="40"
+      fill={currentColor}
+      onClick={onClick}
+    />;
+  </svg>
+};
+
+export default circleSvg
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,16 @@ import Image from "next/image";
 import SevenSegmentLedSample from '@/app/components/SevenSegmentLedSample'
 import SampleButton from '@/app/components/SampleButton'
 import SevenSegmentLed from '@/app/components/SevenSegmentLed'
+import CircleSvg from '@/app/components/CircleSvg'
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <SevenSegmentLed/>
+        <CircleSvg
+          color1="red"
+          color2="blue"
+        />
         <SevenSegmentLedSample></SevenSegmentLedSample>
         <SampleButton></SampleButton>
         <Image
